fc60c2cf67ece805624e597b82bc61c2340bcff9
Malcom3D <Malcom3D@users.noreply.github.com>
2025-06-17 16:05:25 +0200

    8 - sizing tool

--- freecad/StructureTools2/size.py
+++ freecad/StructureTools2/size.py
@@ -29,8 +29,7 @@ def set_type(s):
 
 class SizeTaskPanel:
     def __init__(self, widget, selection):
-#        self.form = [widget, QtGui.QDialog()]
-        self.form = [QtGui.QDialog(), QtGui.QDialog()]
+        self.form = [widget, QtGui.QDialog()]
         layoutStd = QtGui.QVBoxLayout()
         # Standard ComboBox
         self.form[0].setWindowTitle("Building Standard")
