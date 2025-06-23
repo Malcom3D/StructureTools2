8416784b7ebb07c304d79288441cc131b7497956
Malcom3D <Malcom3D@users.noreply.github.com>
2025-06-23 15:06:58 +0200

    12 - sizing tool

--- freecad/StructureTools2/sizing.py
+++ freecad/StructureTools2/sizing.py
@@ -311,7 +311,7 @@ class Sizing:
         layoutDesRes = QtGui.QVBoxLayout()
         for i in range(0,len(self.GammaMList[:])):
             for l in self.GammaMList[i][0].split():
-                if l in self.WoodTypeValue.currentText().lower():
+                if l in self.WoodTypeValue.currentText().split().lower():
                     self.GammaM = self.GammaMList[i][2]
                     print('GammaM :' + self.GammaM)
                     break
