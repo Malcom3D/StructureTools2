f6ea14131fddfda54590bf155591611910b1e1fe
Malcom3D <Malcom3D@users.noreply.github.com>
2025-06-23 21:30:08 +0200

    12 - sizing tool

--- freecad/StructureTools2/sizing.py
+++ freecad/StructureTools2/sizing.py
@@ -346,6 +346,7 @@ class Sizing:
         text = ''
         self.DimCommValue.addItem(text)
         for i in range(0,len(self.BeamDimList[:])):
+                print(self.BeamDimList[i][0], self.bmin, self.BeamDimList[i][1],self.hmin, self.BeamDimList[i][3], self.length)
                 if (self.BeamDimList[i][0] >= self.bmin) and (self.BeamDimList[i][1] >= self.hmin) and (self.BeamDimList[i][3] >= self.length):
                     text = str(self.BeamDimList[i][0]) + 'x' + str(self.BeamDimList[i][1]) + 'x' + str(self.BeamDimList[i][2])
                     self.DimCommValue.addItem(text)
