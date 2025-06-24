1aab1d93efd4a0f74dcf497125c9ca5929a208c7
Malcom3D <Malcom3D@users.noreply.github.com>
2025-06-24 15:20:39 +0200

    12 - sizing tool

--- freecad/StructureTools2/sizing.py
+++ freecad/StructureTools2/sizing.py
@@ -85,6 +85,8 @@ class Sizing:
         self.B = 0
         self.H = 0
 
+        self.beamweight = 0
+
         self.form = QtGui.QDialog()
         self.LoadParam()
 
@@ -573,22 +575,24 @@ class Sizing:
             self.BeamComDimm()
 
     def BeamComDimm(self):
-        self.DimCommValue.clear()
-        for i in range(0,len(self.BeamDimList[:])):
-                text = 'text'
-                if (self.BeamDimList[i][0] >= self.bmin) and (self.BeamDimList[i][1] >= self.hmin) and (self.BeamDimList[i][2] >= self.length):
-                    text = str(self.BeamDimList[i][0]) + 'x' + str(self.BeamDimList[i][1]) + 'x' + str(self.BeamDimList[i][2])
-                    self.DimCommValue.addItem(text)
-        text = 'Custom...'
-        self.DimCommValue.addItem(text)
-        if self.DimCommValue.count() == 1:
-            self.DimCommXValue.show()
-            self.DimCommYValue.show()
-            self.DimCommXValue.setMinimum(self.bmin)
-            self.DimCommYValue.setMinimum(self.hmin)
-        elif self.DimCommValue.count() >= 1 and self.DimCommXValue.isVisibleTo(self.form) and self.DimCommYValue.isVisibleTo(self.form):
-            self.DimCommXValue.hide()
-            self.DimCommYValue.hide()
+        G1tmp = self.beamweight + self.G1avr
+        if self.beamweight == 0 or (G1tmp != self.G1LoadValue.value()):
+            self.DimCommValue.clear()
+            for i in range(0,len(self.BeamDimList[:])):
+                    text = 'text'
+                    if (self.BeamDimList[i][0] >= self.bmin) and (self.BeamDimList[i][1] >= self.hmin) and (self.BeamDimList[i][2] >= self.length):
+                        text = str(self.BeamDimList[i][0]) + 'x' + str(self.BeamDimList[i][1]) + 'x' + str(self.BeamDimList[i][2])
+                        self.DimCommValue.addItem(text)
+            text = 'Custom...'
+            self.DimCommValue.addItem(text)
+            if self.DimCommValue.count() == 1:
+                self.DimCommXValue.show()
+                self.DimCommYValue.show()
+                self.DimCommXValue.setMinimum(self.bmin)
+                self.DimCommYValue.setMinimum(self.hmin)
+            elif self.DimCommValue.count() >= 1 and self.DimCommXValue.isVisibleTo(self.form) and self.DimCommYValue.isVisibleTo(self.form):
+                self.DimCommXValue.hide()
+                self.DimCommYValue.hide()
 
     def selectedDimComm(self):
         index = self.DimCommValue.currentIndex()
@@ -602,7 +606,6 @@ class Sizing:
             if self.DimCommXValue.isVisibleTo(self.form) and self.DimCommYValue.isVisibleTo(self.form):
                 self.DimCommXValue.hide()
                 self.DimCommYValue.hide()
-#        else:
             selection = self.DimCommValue.currentText().split('x')
             self.B = float(selection[0])
             self.H = float(selection[1])
