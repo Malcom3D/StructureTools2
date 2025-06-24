d595e843c745ff4186a8ada3c483a16a4e0ce4a0
Malcom3D <Malcom3D@users.noreply.github.com>
2025-06-24 14:47:09 +0200

    12 - sizing tool

--- freecad/StructureTools2/sizing.py
+++ freecad/StructureTools2/sizing.py
@@ -566,9 +566,9 @@ class Sizing:
                             self.G1LoadValue.setValue(G1tmp)
 
                         if self.DimCommValue.currentText():
-                        print(self.DimCommValue.currentText())
-                        if not (float(self.DimCommValue.currentText().split('x')[0]) >= Width and float(self.DimCommValue.currentText().split('x')[1]) >= Height):
-                        self.BeamComDimm()
+                            print(self.DimCommValue.currentText())
+                            if not (float(self.DimCommValue.currentText().split('x')[0]) >= Width and float(self.DimCommValue.currentText().split('x')[1]) >= Height):
+                                self.BeamComDimm()
 
             self.BeamComDimm()
 
