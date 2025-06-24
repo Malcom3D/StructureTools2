10defb5bdafb63a963593ee929e21027fc0be3c5
Malcom3D <Malcom3D@users.noreply.github.com>
2025-06-24 10:53:56 +0200

    12 - sizing tool

--- freecad/StructureTools2/sizing.py
+++ freecad/StructureTools2/sizing.py
@@ -579,7 +579,7 @@ class Sizing:
                         self.Fd = self.NTC2018Data.FundComb(self.G1LoadValue.value(), self.GammaList[1][4], self.G2LoadValue.value(), self.GammaList[2][4], 0, self.GammaList[3][4])
                         self.bmin, self.hmin = self.NTC2018Data.PreDim(self.Fd, self.BeamStepValue.value(), self.length, self.fmd, self.fvd)
 
-                        if self.B and self.H:
+                        if self.B != 0 and self.H != 0:
                             Width = self.B
                             Height = self.H
                         else:
