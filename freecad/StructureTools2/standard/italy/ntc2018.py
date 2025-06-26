39504b9d73549508c8056ee3b58d25e8b7a23cb2
Malcom3D <Malcom3D@users.noreply.github.com>
2025-06-26 16:02:42 +0200

    12 - sizing tool

--- freecad/StructureTools2/standard/italy/ntc2018.py
+++ freecad/StructureTools2/standard/italy/ntc2018.py
@@ -124,10 +124,9 @@ class NTC2018:
         Area = interaxis*length
         q = Fd*interaxis/Area
         I = (Width*Height**3)/12
-        torb = cos(alpha)
-        if torb == 1:
+        if cos(alpha) == 1:
            f = (5/384)*((q*length**4)/(I*E005)
-        elif torb != (1 and 0):
+        elif cos(alpha) != 1 and cos(alpha) != 0:
            f = (5/384)*(((q*cos(alpha))*length**4)/(I*E005*cos(alpha)**2)
         return f
 
