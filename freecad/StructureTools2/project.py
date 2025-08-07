a6cf3afa12555c179f4360f89263b15df7469a89
Malcom3D <Malcom3D@users.noreply.github.com>
2025-08-07 18:08:37 +0200

    15 - Project land

--- freecad/StructureTools2/project.py
+++ freecad/StructureTools2/project.py
@@ -212,9 +212,9 @@ class Project:
         self.SouthEst = Geodesic.WGS84.Direct(self.center[0],self.center[1],45,dist)
         latNW, longNW = (float(format(self.NordWest['lat2'])),float(format(self.NordWest['lon2'])))
         latSE, longSE = (float(format(self.SouthEst['lat2'])),float(format(self.SouthEst['lon2'])))
-        LandArea = round(float(format(Geodesic.WGS84.Inverse(latNW, longNW, latSE, longSE, Geodesic.AREA)['a12'])), 3)
+        LandArea = float(format(Geodesic.WGS84.Inverse(latNW, longNW, latSE, longSE, Geodesic.AREA)['a12']))
         print('LandArea: ', LandArea)
-        self.LandAreaValueLabel.setText('Land area: ' + str(LandArea) + ' m²')
+        self.LandAreaValueLabel.setText('Land area: ' + str(round(LandArea, 3)) + ' m²')
         
     def ProjectParam(self):
         # ntc2018 Project Parameter QDialog
